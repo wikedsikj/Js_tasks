154cda07f8fb701eb8346be5f7ffb9901be8af92
wikedsikj <33325134+wikedsikj@users.noreply.github.com>
2022-10-19 14:30:57 +0300

    Create task_2.js

---
+++ task_2.js
@@ -0,0 +1,11 @@
+let num = prompt("Enter the number: ");
+let lineSquare = "";
+
+for (let n = 0; n < num; n++){
+    if (!num) break;
+    for (let m = 0; m < num; m++){
+        lineSquare +=  "*";
+    }
+    lineSquare += "\n";
+}
+alert(lineSquare);
