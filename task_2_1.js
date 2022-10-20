e34e0e39f0241270d67ad53671c51fbe0e36393a
wikedsikj <33325134+wikedsikj@users.noreply.github.com>
2022-10-20 11:32:51 +0300

    Update task_2_1.js

--- task_2_1.js
+++ task_2_1.js
@@ -1,9 +1,10 @@
 let num = prompt("Enter the number: ");
 let lineSquare = "";
 
-for (let n = 0; n < num; n++){
+for (let n = 0; n < num; n++) {
     if (!num) break;
-    for (let m = 0; m < num; m++){
+    
+    for (let m = 0; m < num; m++) {
         lineSquare +=  "*";
     }
     lineSquare += "\n";
